e2580dd7c07007a353474e935176ccfd7f01b312
Vadim Fadeev <xboxfadeev@gmail.com>
2024-11-08 13:38:46 +0000

    feat: update addr

--- programs/rarible_editions/src/lib.rs
+++ programs/rarible_editions/src/lib.rs
@@ -3,7 +3,7 @@ use anchor_lang::prelude::*;
 pub mod instructions;
 pub use instructions::*;
 
-declare_id!("DsaxZA54w7N9z8jxobtzy3rhQQmXjngjvJNvX1HubtkJ");
+declare_id!("Rari9ftBd6vFdtpn8TDLwN2ze24KKkM5MLEETNiBMNn");
 
 pub mod errors;
 pub mod state;
--- programs/rarible_marketplace/src/lib.rs
+++ programs/rarible_marketplace/src/lib.rs
@@ -8,7 +8,7 @@ pub mod utils;
 use instructions::*;
 
 // rAREXWkxUP9Cr91tRVJ29NumDAEKvNpDWZNqcfSwBNG - program id
-declare_id!("61AUdCJkaaw2b3Byhp9gLyK6rvt8DTsUoGTKwnvEZMaf");
+declare_id!("Rarim7DMoD45z1o25QWPsWvTdFSSEdxaxriwWZLLTic");
 
 #[program]
 pub mod marketplace {
