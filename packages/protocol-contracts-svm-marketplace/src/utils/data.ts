f3394d8a51dd9605538a47ea51ca5d713cb4335a
Vadim Fadeev <xboxfadeev@gmail.com>
2024-11-08 15:13:12 +0000

    feat: fix build

--- packages/protocol-contracts-svm-marketplace/src/utils/data.ts
+++ packages/protocol-contracts-svm-marketplace/src/utils/data.ts
@@ -1,12 +1,14 @@
+import { Provider } from "@coral-xyz/anchor";
+
 // Fetch order
 export const fetchOrderByAddress = async (provider: Provider, orderAddress: string) => {
-	const marketplaceProgram = getMarketplaceProgram(provider);
+	// const marketplaceProgram = getMarketplaceProgram(provider);
 
-	try {
-		const orderAccount = await marketplaceProgram.account.order.fetch(orderAddress);
+	// try {
+	// 	const orderAccount = await marketplaceProgram.account.order.fetch(orderAddress);
 
-		return orderAccount;
-	} catch (e) {
-		return undefined;
-	}
+	// 	return orderAccount;
+	// } catch (e) {
+	// 	return undefined;
+	// }
 };
