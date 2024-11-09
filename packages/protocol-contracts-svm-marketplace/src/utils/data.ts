f7e8d69aa6ebfdccdbb531a8ae73ef5cf546905c
Vadim Fadeev <xboxfadeev@gmail.com>
2024-11-09 06:29:27 +0000

    Feature/market cli (#11)
    * feat: switch to common js

--- packages/protocol-contracts-svm-marketplace/src/utils/data.ts
+++ packages/protocol-contracts-svm-marketplace/src/utils/data.ts
@@ -1,14 +1,24 @@
-import { Provider } from "@coral-xyz/anchor";
+import { AnchorProvider } from "@coral-xyz/anchor";
+import { getRaribleMarketplaceProgram } from "@rarible_int/protocol-contracts-svm-core";
 
 // Fetch order
-export const fetchOrderByAddress = async (provider: Provider, orderAddress: string) => {
-	// const marketplaceProgram = getMarketplaceProgram(provider);
+export const fetchOrderByAddress = async (provider: AnchorProvider, orderAddress: string) => {
+	const marketplaceProgram = getRaribleMarketplaceProgram(provider)
+	try {
+		const orderAccount = await marketplaceProgram.account.order.fetch(orderAddress);
+		return orderAccount;
+	} catch (e) {
+		return undefined;
+	}
+};
 
-	// try {
-	// 	const orderAccount = await marketplaceProgram.account.order.fetch(orderAddress);
-
-	// 	return orderAccount;
-	// } catch (e) {
-	// 	return undefined;
-	// }
+// Fetch market
+export const fetchMarketByAddress = async (provider: AnchorProvider, marketAddress: string) => {
+	const marketplaceProgram = getRaribleMarketplaceProgram(provider);
+	try {
+		const marketAccount = await marketplaceProgram.account.market.fetch(marketAddress);
+		return marketAccount;
+	} catch (e) {
+		return undefined;
+	}
 };
