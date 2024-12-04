461db379516042fead0d11c81a223ea9b655f902
Vadim Fadeev <xboxfadeev@gmail.com>
2024-12-04 12:22:46 +0000

    feat: add prod deploy

--- packages/protocol-contracts-svm-marketplace/scripts/initMarket.sh
+++ packages/protocol-contracts-svm-marketplace/scripts/initMarket.sh
@@ -15,3 +15,11 @@ npx ts-node ./src/cli/initMarket.ts \
   -m Rari4ReeeT8bhbsRGP5J8RBhTFXTAP7iMm7VHuNQTs5 \
   --feeBps 500 \
   --feeRecipient AsSKqK7CkxFUf3KaoQzzr8ZLPm5fFguUtVE5QwGALQQn
+
+npx ts-node ./src/cli/initMarket.ts \
+  -k ~/.config/solana/id.json \
+  -r https://mainnetbeta-rpc.eclipse.xyz \
+  -m Rari4ReeeT8bhbsRGP5J8RBhTFXTAP7iMm7VHuNQTs5 \
+  --feeBps 500 \
+  --feeRecipient AsSKqK7CkxFUf3KaoQzzr8ZLPm5fFguUtVE5QwGALQQn \
+  --ledger true
